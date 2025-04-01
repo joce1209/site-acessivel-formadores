b03fd091a0be2f81837230e1fd2691a2df4fcb2a
Jocemara <130997343+joce1209@users.noreply.github.com>
2025-04-01 13:48:59 -0300

    arrumei script

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     aumentaFonteBotao.addEventListener('click', function () {
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
 
-        diminuirFonteBotao.addEventListener('click', function () {
-            tamanhoAtualFonte -= 0.1;
-            document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
 
